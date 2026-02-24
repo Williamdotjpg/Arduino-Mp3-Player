b6b10f061d8f183e8047b1371104914f5615610f
Holmes <williamholmes678@gmail.com>
2026-02-24 13:33:21 -0500

    Added Devs and Resources

--- mp3playercode/mp3playercode.ino
+++ mp3playercode/mp3playercode.ino
@@ -1,3 +1,14 @@
+/*
+Developers: Will Holmes, Cyle Krohling
+
+resources: https://www.oceanlabz.in/getting-started-with-serial-mp3-player/
+
+
+
+
+*/
+
+
 void setup() {
   // put your setup code here, to run once:
 
