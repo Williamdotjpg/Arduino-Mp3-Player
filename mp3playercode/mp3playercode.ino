9e140d3c9d232c4e8467de31b1ead2ab74e0cf0c
Holmes <williamholmes678@gmail.com>
2026-02-26 14:06:55 -0500

    God rid of useless comments untested code

--- mp3playercode/mp3playercode.ino
+++ mp3playercode/mp3playercode.ino
@@ -10,61 +10,39 @@ http://github.com/salvadorrueda/SerialMP3Player/tree/master
 
 */
 
-/*
-  LiquidCrystal Library - Hello World
 
- Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
- library works with all LCD displays that are compatible with the
- Hitachi HD44780 driver. There are many of them out there, and you
- can usually tell them by the 16-pin interface.
-
- This sketch prints "Hello World!" to the LCD
- and shows the time.
-
-  The circuit:
- * LCD RS pin to digital pin 12
- * LCD Enable pin to digital pin 11
- * LCD D4 pin to digital pin 5
- * LCD D5 pin to digital pin 4
- * LCD D6 pin to digital pin 3
- * LCD D7 pin to digital pin 2
- * LCD R/W pin to ground
- * LCD VSS pin to ground
- * LCD VCC pin to 5V
- * 10K resistor:
- * ends to +5V and ground
- * wiper to LCD VO pin (pin 3)
-
- Library originally added 18 Apr 2008
- by David A. Mellis
- library modified 5 Jul 2009
- by Limor Fried (http://www.ladyada.net)
- example added 9 Jul 2009
- by Tom Igoe
- modified 22 Nov 2010
- by Tom Igoe
- modified 7 Nov 2016
- by Arturo Guadalupi
-
- This example code is in the public domain.
-
- https://docs.arduino.cc/learn/electronics/lcd-displays
-
-*/
 
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+#include <SoftwareSerial.h>
 
-LiquidCrystal_I2C lcd(0x27, 16, 2);  // Address, columns, rows
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+SoftwareSerial mp3(10, 11); // RX, TX
+
+byte queryTrack[] = {0x7E,0xFF,0x06,0x4C,0x00,0x00,0x00,0xFE,0xB0,0xEF};
 
 void setup() {
   lcd.init();
   lcd.backlight();
-  lcd.setCursor(0, 0);
-  lcd.print("bye");
-  lcd.setCursor(0, 1);
-  lcd.print("William");
+  mp3.begin(9600);
 }
 
 void loop() {
+  mp3.write(queryTrack, 10);
+  delay(200);
+
+  if (mp3.available() >= 10) {
+    byte response[10];
+    for (int i = 0; i < 10; i++) {
+      response[i] = mp3.read();
+    }
+
+    int trackNumber = response[6];  // low byte
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Track: ");
+    lcd.print(trackNumber);
+  }
+
+  delay(1000);
 }
