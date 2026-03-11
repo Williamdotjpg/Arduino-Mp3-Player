69be0496fbbf3ed67a4f95d096c6fc6f470e0a5c
Holmes <williamholmes678@gmail.com>
2026-03-11 12:43:17 -0400

    Pause button sorta works Claude link updated

--- mp3playercode/mp3playercode.ino
+++ mp3playercode/mp3playercode.ino
@@ -8,6 +8,7 @@ http://github.com/salvadorrueda/SerialMP3Player/tree/master
 LCD Troubleshooting Code:https://chatgpt.com/share/69a5dca7-a304-8002-a3b0-b455103b4850
 B10K: https://chatgpt.com/share/69a5e2ae-5de0-8002-92cb-9df7dc2da5c6
 Buttons: https://claude.ai/share/514c4088-8de8-4913-9fd6-031d9d5ecccd
+https://docs.arduino.cc/built-in-examples/digital/Button/ 
 
 */
 
@@ -43,7 +44,6 @@ Buttons: https://claude.ai/share/514c4088-8de8-4913-9fd6-031d9d5ecccd
 
   by Salvador Rueda
  *******************************************************************************/
-
 #include "SerialMP3Player.h" 
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
@@ -53,21 +53,199 @@ Buttons: https://claude.ai/share/514c4088-8de8-4913-9fd6-031d9d5ecccd
 #define POT A0
 
 // --- Button pins ---
-#define BTN_NEXT       11
-#define BTN_PREV       9
-#define BTN_PLAYPAUSE  10
+#define BTN_NEXT       A1
+#define BTN_PREV       A3
+#define BTN_PLAYPAUSE  A2
 
-// Upload this alone to test your buttons
+SerialMP3Player mp3(RX, TX);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+char c;
+char cmd=' ';
+char cmd1=' ';
+int lastVolume = -1;
+
+void menu(char op, int nval);
+void decode_c();
+void checkButtons();
+
 void setup() {
   Serial.begin(9600);
-  pinMode(11, INPUT_PULLUP);
-  pinMode(9, INPUT_PULLUP);
-  pinMode(10, INPUT_PULLUP);
+  mp3.begin(9600);
+  delay(500);
+
+  mp3.sendCommand(CMD_SEL_DEV, 0, 2);
+  delay(500);
+
+  pinMode(BTN_NEXT,      INPUT_PULLUP);
+  pinMode(BTN_PREV,      INPUT_PULLUP);
+  pinMode(BTN_PLAYPAUSE, INPUT_PULLUP);
+
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0,0);
+  lcd.print("MP3 Ready");
 }
 
 void loop() {
-  if (digitalRead(11) == LOW) Serial.println("NEXT pressed");
-  if (digitalRead(9) == LOW) Serial.println("PREV pressed");
-  if (digitalRead(109) == LOW) Serial.println("PLAY pressed");
-  delay(100);
+  static unsigned long lastQuery = 0;
+
+  // Potentiometer volume
+  int potValue = analogRead(POT);
+  int volume = map(potValue, 0, 1023, 0, 30);
+  if (volume != lastVolume) {
+    mp3.setVol(volume);
+    lastVolume = volume;
+    lcd.setCursor(0,1);
+    lcd.print("Vol: ");
+    lcd.print(volume);
+    lcd.print("   ");
+  }
+
+  if (millis() - lastQuery > 2000) {
+    mp3.qPlaying();
+    lastQuery = millis();
+  }
+
+  checkButtons();
+
+  if (Serial.available()) {
+    c = Serial.read();
+    decode_c();
+  }
+
+  if (mp3.available()) {
+    String answer = mp3.decodeMP3Answer();
+    Serial.println(answer);
+    if (answer.indexOf("Playing:") >= 0) {
+      int track = answer.substring(answer.lastIndexOf(" ") + 1).toInt();
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print("Track: ");
+      lcd.print(track);
+      lcd.setCursor(0,1);
+      lcd.print("Vol: ");
+      lcd.print(lastVolume);
+    }
+  }
+}
+
+void checkButtons() {
+  const unsigned long DOUBLE_CLICK_WINDOW = 400;
+  const unsigned long DEBOUNCE = 50;
+
+  static unsigned long lastNextBounce  = 0;
+  static unsigned long lastPrevBounce  = 0;
+  static unsigned long lastPlayBounce  = 0;
+  static unsigned long lastNextPress   = 0;
+  static unsigned long lastPrevPress   = 0;
+  static bool nextPending = false;
+  static bool prevPending = false;
+  static bool isPlaying   = true;
+
+  unsigned long now = millis();
+
+  // --- NEXT button ---
+  if (digitalRead(BTN_NEXT) == LOW && now - lastNextBounce > DEBOUNCE) {
+    lastNextBounce = now;
+
+    if (nextPending && (now - lastNextPress < DOUBLE_CLICK_WINDOW)) {
+      mp3.playNext();
+      nextPending = false;
+      isPlaying = true;
+      lcd.setCursor(0,0);
+      lcd.print(">> Next Track   ");
+    } else {
+      nextPending = true;
+      lastNextPress = now;
+    }
+
+    while (digitalRead(BTN_NEXT) == LOW);
+  }
+
+  if (nextPending && (now - lastNextPress > DOUBLE_CLICK_WINDOW)) {
+    mp3.sendCommand(0x1C, 0, 0);
+    nextPending = false;
+    lcd.setCursor(0,0);
+    lcd.print("+10s            ");
+  }
+
+  // --- PREV button ---
+  if (digitalRead(BTN_PREV) == LOW && now - lastPrevBounce > DEBOUNCE) {
+    lastPrevBounce = now;
+
+    if (prevPending && (now - lastPrevPress < DOUBLE_CLICK_WINDOW)) {
+      mp3.playPrevious();
+      prevPending = false;
+      isPlaying = true;
+      lcd.setCursor(0,0);
+      lcd.print("<< Prev Track   ");
+    } else {
+      prevPending = true;
+      lastPrevPress = now;
+    }
+
+    while (digitalRead(BTN_PREV) == LOW);
+  }
+
+  if (prevPending && (now - lastPrevPress > DOUBLE_CLICK_WINDOW)) {
+    mp3.sendCommand(0x1B, 0, 0);
+    prevPending = false;
+    lcd.setCursor(0,0);
+    lcd.print("-10s            ");
+  }
+
+  // --- PLAY/PAUSE button ---
+  if (digitalRead(BTN_PLAYPAUSE) == LOW && now - lastPlayBounce > DEBOUNCE) {
+    lastPlayBounce = now;
+
+    if (isPlaying) {
+      mp3.pause();
+      isPlaying = false;
+      lcd.setCursor(0,0);
+      lcd.print("Paused          ");
+    } else {
+      mp3.play();
+      isPlaying = true;
+      lcd.setCursor(0,0);
+      lcd.print("Playing         ");
+    }
+
+    while (digitalRead(BTN_PLAYPAUSE) == LOW);
+  }
+}
+
+void menu(char op, int nval){
+  switch (op){
+    case 'P': mp3.play(nval); break;
+    case 'F': mp3.playF(nval); break;
+    case 'S': mp3.playSL(nval); break;
+    case 'p': mp3.play(); break;
+    case 'a': mp3.pause(); break;
+    case 's': mp3.stop(); break;
+    case '>': mp3.playNext(); break;
+    case '<': mp3.playPrevious(); break;
+    case '+': mp3.volUp(); break;
+    case '-': mp3.volDown(); break;
+    case 'v': mp3.setVol(nval); break;
+    case 'c': mp3.qPlaying(); break;
+  }
+}
+
+void decode_c(){
+  if (c=='v' || c=='P' || c=='F' || c=='S'){
+    cmd=c;
+  }
+  else if(c>='0' && c<='9'){
+    if(cmd1==' '){
+      cmd1 = c;
+    }else{
+      menu(cmd, ((cmd1-'0')*10)+(c-'0'));
+      cmd = ' ';
+      cmd1 = ' ';
+    }
+  }
+  else{
+    menu(c,0);
+  }
 }
